67025f84f1500436c379bcc52f5e4dee21d2260a
AlanPonce11 <45779828+AlanPonce11@users.noreply.github.com>
2022-12-01 16:59:51 -0600

    Add files via upload

---
+++ PythonCourse/Python Curso 28_11_22.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Repaso general de conceptos y sintaxis de la programación con Python 3 (28/11/22)
+
+# # Python es un lenguaje de programación de alto nivel que se utiliza para desarrollar aplicaciones de todo tipo. 
+
+# # Python, sino que se ejecutan directamente por el ordenador utilizando un programa denominado interpretador, por lo que no es necesario “traducirlo” a lenguaje máquina.
+# 
+# 
+
+# # Instalacion de Python (28/11/22)
+
+# # https://www.python.org/downloads/
+# 
+# 
+# 
+
+# # https://www.anaconda.com/products/distribution
+
+# # https://visualstudio.microsoft.com/es/downloads/
+
+# # https://www.programiz.com/python-programming/online-compiler/
+
+# # Programacion con python (28/11/22)
+
+# In[31]:
+
+
+print ("Hello world")
+
+
+# # Tipo de datos
+
+# In[32]:
+
+
+#enteros
+print(10000)
+#float
+print(22.5)
+#cadenas (strings)
+print("Hola a todos")
+#boolean
+print(True)
+print(False)
+
+
+# # Operaciones basicas
+
+# In[33]:
+
+
+#suma
+print(5 + 100)
+#resta
+print(5 - 100)
+#slash
+print(50 // 5)
+#multiplication
+print(5 * 10)
+#Exponenciacion
+print(2 ** 4)
+
+
+# # Operaciones booleanos
+
+# In[34]:
+
+
+# True
+# False
+
+print ( 5 <= 4 )
+print ( 5 >= 4 )
+print ( 4 == 4 )
+print ( 5 != 4 )
+
+
+# # Variables
+
+# In[35]:
+
+
+caja1 = "Daniel"
+print  (caja1)
+
+caja1=123
+print  (caja1)
+
+
+# # Condicionales
+
+# In[36]:
+
+
+esta_lloviendo = True
+
+if  esta_lloviendo:
+	print("quedarme en casa")
+else: 
+    print("salir al parque")
+
+
+# In[37]:
+
+
+esta_lloviendo = False
+
+if  esta_lloviendo:
+	print("quedarme en casa")
+else: 
+    print("salir al parque")
+
+
+# In[38]:
+
+
+esta_lloviendo = True
+
+if  esta_lloviendo:
+	print("quedarme en casa")
+elif esta_soleado:
+	print("salir al parque")
+else: 
+    print("quedarme en casa")
+
+
+# # Fin Clase 28/11/22
+
+# # Inicio Clase  30/11/22
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
